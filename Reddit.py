c78cc0bcafee2ab98f842211606cac600e0dab25
Kevin <kjh7796@gmail.com>
2016-10-19 00:35:41 -0400

    -Added Reddit imgur hosting support

--- Reddit.py
+++ Reddit.py
@@ -26,21 +26,23 @@ while True:
     sub = 'HighRes'
     submissions = r.get_subreddit(sub).get_random_submission()
     print('Submission Found in ' + sub)
+
     url = urllib.parse.unquote(submissions.url)
     reddit = 'reddit'
     jpg = 'jpg'
     imgur = 'imgur'
     if jpg in url and imgur in url:
         newurl = url.split('/')[-1].split('.')[0]
-        print('Downlading Image')
+        print('Downlading Image through Imgur')
         im.get_image(newurl).download(path="C:\\Users\\KEVIN\\Pictures\\PyImgur", name=id_gen(), overwrite=False, size=None)
-        time.sleep(30)
+
 
 
         #Space
     if reddit in url:
-        request.urlretrieve(url, "C:\\Users\\KEVIN\\Pictures\\PyImgur")
-        time.sleep(30)
+        print("Downloading through I.Reddit")
+        request.urlretrieve(url, "C:\\Users\\KEVIN\\Pictures\\PyImgur" + id_gen() + '.jpg')
+
 
 
 

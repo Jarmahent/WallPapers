20109f47f6b01acae2cc90b56a4821f8b8198991
Kevin <kjh7796@gmail.com>
2017-06-16 23:43:18 -0400

    -Added a timer for 30 seconds because the api doesnt like too many request. -Added a filter for duplicate urls, although(rare) you will still get duplicate images

--- Reddit.py
+++ Reddit.py
@@ -4,26 +4,10 @@ import pyimgur
 import urllib.parse
 import string
 import random
-import urlArray
-import time
 from urllib import request
-from PIL import Image
-import sqlite3
+import time
 
 keyWords = ['gallery', '/a/']
-
-
-class get_image(object):
-
-    def width(self):
-        im = Image.open(self)
-        width = im.size[0]
-        return width
-
-    def height(self):
-        im = Image.open(self)
-        height = im.size[1]
-        return height
 
 sub = 'wallpaper'
 redd = "redd"
@@ -32,87 +16,103 @@ jpg = 'jpg'
 imgur = 'imgur'
 
 
-
 print('Logging in')
-r = praw.Reddit(Obot.app_id)
+r = praw.Reddit(
+    client_id=Obot.app_id,
+    client_secret=Obot.app_secret,
+    user_agent=Obot.app_id
+)
+
+
 im = pyimgur.Imgur(Obot.client_id, Obot.client_secret)
 
-def id_gen(size=9, chars=string.ascii_uppercase + string.digits):  #Create random ID
+
+def id_gen(size=9, chars=string.ascii_uppercase + string.digits):  # Create random ID
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-
-
 while True:
-    print("--Sleeping for 30 seconds")
-    time.sleep(30)
-
-    submissions = r.get_subreddit(sub).get_random_submission()
-
-    print('--Submission Found in ' + sub)
-    print("--Submission.url: ", submissions.url)
-
-    if submissions.url in urlArray.urllist:
-        print("--This url has already been downloaded")
-        continue
-
-    print("--Adding url to array")
-    urlArray.urllist.append(submissions.url)
 
 
+    submissions = r.subreddit(sub).random()
+
+    print('Submission Found in ' + sub)
+    print("Submission.url: ", submissions.url)
 
     url = urllib.parse.unquote(submissions.url)
-    #if url doesnt have jpg and doesnt have "gallery" or "/a/"
-    if(not jpg in url and imgur in url and not "gallery" in url and not "/a/" in url):
-        print("--Url without jpg format: ", url)
+    # if url doesnt have jpg and doesnt have "gallery" or "/a/"
+    if (not jpg in url and imgur in url and not "gallery" in url and not "/a/" in url):
+        print("Url without jpg format: ", url)
         jpgURL = submissions.url + ".jpg"
-        print("--Remade url is: ", jpgURL)
+        print("Remade url is: ", jpgURL)
+        print("Its not in here!")
         imgur_name2 = id_gen()
-        newurl2 = jpgURL.split('/')[-1].split('.')[0]
-        im.get_image(newurl2).download(path="C:\\Users\\KEVIN\\Pictures\\temp_pics", name=imgur_name2, overwrite=False, size=None)
-        print("--Downloaded remade url")
+        ImageDbIn = open("imagedb.txt", "r")
+        if(jpgURL in ImageDbIn):
+            print("URL has already been downloaded, restarting.")
+            break
+        else:
+            print("Appending url to DB")
+            ImageDbOut = open("imagedb.txt", "a")
+            ImageDbOut.write(jpgURL)
+            newurl2 = jpgURL.split('/')[-1].split('.')[0]
+            im.get_image(newurl2).download(path="C:\\Users\\KEVIN\\Pictures\\pics", name=imgur_name2, overwrite=False, size=None)
+            print("Downloaded remade url")
 
+
+    #download image through Imgur
     if jpg in url and imgur in url:
-
 
         newurl = url.split('/')[-1].split('.')[0]
         imgur_name = id_gen()
+        print("New URL detected, storing.")
         temp_pic = imgur_name + ".jpg"
-        print("--Downloading Image through Imgur")
-        im.get_image(newurl).download(path="C:\\Users\\KEVIN\\Pictures\\temp_pics", name=imgur_name, overwrite=False, size=None)
-
-
+        print("\n" + 'Downloading Image through Imgur', "\n")
+        ImageDbIn = open("imagedb.txt", "r")
+        if(newurl in ImageDbIn):
+            print("URL has already been downloaded, restarting.")
+            break
+        else:
+            print("Appending url to DB")
+            ImageDbOut = open("imagedb.txt", "a")
+            ImageDbOut.write(newurl)
+            im.get_image(newurl).download(path="C:\\Users\\KEVIN\\Pictures\\pics", name=imgur_name,
+                                          overwrite=False,
+                                          size=None)
 
     if reddit in url:
         reddit_gen = id_gen()
         reddit_pic = reddit_gen + ".jpg"
-        print("\n" + "--Downloading through I.Reddit")
-        request.urlretrieve(url, "C:\\Users\\KEVIN\\Pictures\\temp_pics\\" + reddit_pic)
+        print("\n" + "Downloading through I.Reddit")
+        print("New URL detected, storing.")
+
+        ImageDbIn = open("imagedb.txt", "r")
+        if(ImageDbIn in url):
+            print("URL has already been downloaded, restarting.")
+            break
+        else:
+            print("Appending url to DB")
+            ImageDbOut = open("imagedb.txt", "a")
+            ImageDbOut.write(url)
+            request.urlretrieve(url, "C:\\Users\\KEVIN\\Pictures\\pics\\" + reddit_pic)
 
     if not reddit in url and not redd in url and not imgur in url and jpg in url:
-        print("--Not reddit or imgur but I can download the image")
+        print("Not reddit or imgur but I can download the image")
         randomGen = id_gen()
-        request.urlretrieve(url, "C:\\Users\\KEVIN\\Pictures\\temp_pics\\" + randomGen + ".jpg")
-        print("--Downloaded", "This image ID is: ", randomGen)
+        print("New URL detected, storing.")
+        ImageDbIn = open("imagedb.txt", "r")
+        if(url in ImageDbIn):
+            print("URL has already been downloaded, restarting.")
+            break
+        else:
+            print("Appending url to DB")
+            ImageDbOut = open("imagedb.txt", "a")
+            ImageDbOut.write(url)
+            request.urlretrieve(url, "C:\\Users\\KEVIN\\Pictures\\pics\\" + randomGen + ".jpg")
+            print("Downloaded", "This image ID is: ", randomGen)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print("Waiting 10 seconds")
+    x = 0
+    for x in range(10):
+        print(x + 1)
+        time.sleep(1)
